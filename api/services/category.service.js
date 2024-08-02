f23aa19549e95cad83a2ed388f55aa5e2c37ff50
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2024-08-01 19:08:41 -0500

    Update product and category schemas and services

--- api/services/category.service.js
+++ api/services/category.service.js
@@ -1,3 +1,4 @@
+const { models } = require('../libs/sequelize');
 // const boom = require('@hapi/boom');
 
 class CategoryService {
@@ -5,15 +6,20 @@ class CategoryService {
   constructor(){
   }
   async create(data) {
-    return data;
+    const newCategory = await models.Category.create(data)
+    return newCategory;
   }
 
   async find() {
-    return [];
+    const categories = await models.Category.findAll()
+    return categories;
   }
 
   async findOne(id) {
-    return { id };
+    const category = await models.Category.findByPk(id, {
+      include: ['products']
+    });
+    return category;
   }
 
   async update(id, changes) {
